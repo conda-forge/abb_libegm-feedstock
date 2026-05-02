5b407789a9fd5fabbb68686ea5e43f3b1c784386
Carlo Rosso <carlo.rosso@iit.it>
2026-05-02 23:01:49 +0200

    add_aarch64 (#2)
    * add aarch64
    
    * MNT: Re-rendered with conda-smithy 3.61.2 and conda-forge-pinning 2026.05.02.00.12.0
    
    ---------
    
    Co-authored-by: conda-forge-webservices[bot] <91080706+conda-forge-webservices[bot]@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 mkdir build && cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -G Ninja
+cmake ${CMAKE_ARGS} .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DCMAKE_BUILD_TYPE=Release -G Ninja
 cmake --build .
 cmake --install .
