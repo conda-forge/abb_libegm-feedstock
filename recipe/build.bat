b3e41f6fc37021f7af8942b550dd74d351b6a0d7
Carlo Rosso <carlo.rosso@iit.it>
2026-05-07 10:41:22 +0200

    patch windows build

--- recipe/build.bat
+++ recipe/build.bat
@@ -1,5 +1,19 @@
 mkdir build
+if errorlevel 1 exit /b 1
+
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" -DCMAKE_BUILD_TYPE=Release -G Ninja
+if errorlevel 1 exit /b 1
+
+cmake .. ^
+  -DCMAKE_INSTALL_PREFIX="%PREFIX%\Library" ^
+  -DCMAKE_BUILD_TYPE=Release ^
+  -DCMAKE_CXX_STANDARD=17 ^
+  -DCMAKE_CXX_STANDARD_REQUIRED=ON ^
+  -G Ninja
+if errorlevel 1 exit /b 1
+
 cmake --build .
+if errorlevel 1 exit /b 1
+
 cmake --install .
+if errorlevel 1 exit /b 1
